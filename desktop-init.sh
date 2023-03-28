ae90f3a885d905e6ba496d8f86b7ad186a7442ec
yx <yxing.xyz@gmail.com>
2023-03-28 20:56:04 +0800

    add peek

--- desktop-init.sh
+++ desktop-init.sh
@@ -50,7 +50,7 @@ emerge -u x11-drivers/xf86-input-libinput x11-drivers/xf86-video-amdgpu acpi \
        www-client/google-chrome app-editors/vscode app-i18n/ibus-rime net-im/telegram-desktop-bin feh scrot media-gfx/flameshot \
        gnome-base/gnome-keyring seahorse gnome-extra/nm-applet lxde-base/lxappearance media-fonts/nerd-fonts media-fonts/source-han-mono \
        media-fonts/source-han-sans media-fonts/source-han-serif scrot vlc mpv app-containers/podman media-sound/netease-cloud-music \
-       app-text/calibre
+       app-text/calibre peek
 
 ## design
 emerge -u krita gimp mypaint
