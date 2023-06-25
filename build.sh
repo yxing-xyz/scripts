4a0d53233e3a7dd77051627dcbcc258d337ec9f2
yx <yxing.xyz@gmail.com>
2023-06-25 10:13:45 +0800

    调整调度时间

--- build.sh
+++ build.sh
@@ -4,18 +4,19 @@ set -o errexit
 
 echo '%wheel ALL=(ALL:ALL) NOPASSWD:ALL' > /etc/sudoers.d/wheel
 
+
 su x -c '
 export GO111MODULE=on
 export GOPROXY=https://goproxy.cn
 
-yay -S trzsz --nouseask --needed --noconfirm --overwrite "*"
-
 yay -Syu --nouseask --needed --noconfirm --overwrite "*"
 '
 
-rm -rf /rootfs/var/lib/pacman/sync/*
-rm -rf /home/x/.*
-rm -rf /home/x/*
-rm -rf /tmp/*
+rm -rf /rootfs/var/lib/pacman/sync/* || true
+rm -rf /home/x/.* || true
+rm -rf /home/x/* || true
+rm -rf /tmp/* || true
+
+
 
 echo '%wheel ALL=(ALL:ALL) ALL' > /etc/sudoers.d/wheel
