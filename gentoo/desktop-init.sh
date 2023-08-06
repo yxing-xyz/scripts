50764b28a5b7ae1b93706179c83a1cb1313440b5
yx <yxing.xyz@gmail.com>
2023-08-06 13:25:11 +0800

    gentoo rustup

--- gentoo/desktop-init.sh
+++ gentoo/desktop-init.sh
@@ -145,3 +145,7 @@ cp -f  ./config/.asoundrc /home/x
 
 echo 'auth       optional     pam_gnome_keyring.so' >> /etc/pam.d/login
 echo 'session    optional     pam_gnome_keyring.so auto_start' >> /etc/pam.d/login
+
+# rustup
+# rustup-init
+# rustup-init-gentoo -s
