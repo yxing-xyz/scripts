48cd2cd07279847b3dab67e0d95fdeabfabfe431
shawnyyu <shawnyyu@tencent.com>
2024-01-03 18:27:11 +0800

    切换源到南京大学源

--- centos7/rootfs.sh
+++ centos7/rootfs.sh
@@ -2,13 +2,13 @@
 
 if [[ $(uname -a) == *"x86_64"* ]]; then
     sed -e 's|^mirrorlist=|#mirrorlist=|g' \
-        -e 's|^#baseurl=http://mirror.centos.org/centos|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos|g' \
+        -e 's|^#baseurl=http://mirror.centos.org/centos|baseurl=https://mirror.nju.edu.cn/centos|g' \
         -i.bak \
         /etc/yum.repos.d/CentOS-*.repo
 else
     sed -e 's|^mirrorlist=|#mirrorlist=|g' \
-        -e 's|^#baseurl=http://mirror.centos.org/altarch/|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-altarch/|g' \
-        -e 's|^#baseurl=http://mirror.centos.org/$contentdir/|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-altarch/|g' \
+        -e 's|^#baseurl=http://mirror.centos.org/altarch/|baseurl=https://mirror.nju.edu.cn/centos-altarch/|g' \
+        -e 's|^#baseurl=http://mirror.centos.org/$contentdir/|baseurl=https://mirror.nju.edu.cn/centos-altarch/|g' \
         -i.bak \
         /etc/yum.repos.d/CentOS-*.repo
 fi
