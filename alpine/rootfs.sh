cbf6167a55ad50324dfc260fd943172a807ecee6
yx <yxing.xyz@gmail.com>
2024-01-30 16:27:39 +0800

    Update rootfs.sh
    Update rootfs.sh
    
    Update rootfs.sh
    
    Update rootfs.sh

--- alpine/rootfs.sh
+++ alpine/rootfs.sh
@@ -5,9 +5,13 @@ echo 'https://mirrors.nju.edu.cn/alpine/edge/testing' >>/etc/apk/repositories
 
 apk update
 
-apk add gcc g++ make automake autoconf libtool perl bash git lrzsz procps
+apk add gcc gdb g++ make automake autoconf libtool perl bash git lrzsz procps tzdata
 apk add openssh-server zlib-dev openssl-dev pcre-dev pcre2-dev
 apk add tcpdump lsof net-tools bind-tools mtr wget curl
+
+# dev
+apk add pipx lazygit mycli pgcli 
+pipx install iredis  
 
 sed -i 's/[# ]*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 ssh-keygen -A
