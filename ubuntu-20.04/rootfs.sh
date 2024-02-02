2ebdc996ea935aca0ae94381f5006cc1452846ca
shawnyyu <shawnyyu@tencent.com>
2024-02-02 17:41:37 +0800

    ubuntu

--- ubuntu-20.04/rootfs.sh
+++ ubuntu-20.04/rootfs.sh
@@ -1,10 +1,13 @@
-#!/bin/env bash
-
+#!/bin/sh
 export DEBIAN_FRONTEND=noninteractive
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-# debian 12容器修改了配置文件格式
-sed -i 's/deb.debian.org/mirrors.nju.edu.cn/g' /etc/apt/sources.list.d/debian.sources
+echo "Asia/Shanghai" >/etc/timezone
 
+if [[ $(uname -a) == *"x86_64"* ]]; then
+    sed 's|archive.ubuntu.com|mirrors.nju.edu.cn|' -i /etc/apt/sources.list
+    sed 's|security.ubuntu.com|mirrors.nju.edu.cn|' -i /etc/apt/sources.list
+else
+    sed 's|ports.ubuntu.com|mirrors.nju.edu.cn|' -i /etc/apt/sources.list
+fi
 apt update
 apt upgrade
 
@@ -12,9 +15,7 @@ apt install -y gcc g++ make automake autoconf libtool perl bash git lrzsz procps
 apt install -y openssh-server zlib1g-dev libssl-dev libpcre2-dev libpcre3-dev
 apt install -y tcpdump lsof net-tools bind9-utils bind9-dnsutils mtr wget curl
 
-sed -i 's/[# ]*UsePAM.*/UsePAM no/' /etc/ssh/sshd_config
 sed -i 's/[# ]*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-mkdir /var/run/sshd
 ssh-keygen -A
 
 echo 'root:root' | chpasswd
