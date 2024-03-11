ea3727368c8c62a6b2fa3ca9c451176451fe22b3
shawnyyu <shawnyyu@tencent.com>
2024-03-11 14:32:40 +0800

    调整centos目录结构

---
+++ centos/centos7/rootfs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+yum clean all && yum makecache
+yum update -y
+
+yum install -y gcc gcc-c++ make automake autoconf libtool perl bash git lrzsz procps \
+    sudo vim tmux
+yum install -y openssh-server zlib-devel openssl-devel pcre-devel
+yum install -y tcpdump lsof net-tools bind-utils mtr wget curl
+
+sed -i 's/[# ]*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+ssh-keygen -A
+echo 'root:root' | chpasswd
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+# china
+sed -e 's|^mirrorlist=|#mirrorlist=|g' \
+    -e 's|^#baseurl=http://mirror.centos.org/centos|baseurl=https://mirrors.nju.edu.cn/centos|g' \
+    -i.bak \
+    /etc/yum.repos.d/CentOS-*.repo
---
+++ centos/centos8/rootfs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+yum clean all && yum makecache
+yum update -y
+
+yum install -y gcc gcc-c++ make automake autoconf libtool perl bash git lrzsz procps \
+    sudo vim tmux
+yum install -y openssh-server zlib-devel openssl-devel pcre-devel
+yum install -y tcpdump lsof net-tools bind-utils mtr wget curl
+
+sed -i 's/[# ]*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+ssh-keygen -A
+echo 'root:root' | chpasswd
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+# mirror
+sed -e "s|^mirrorlist=|#mirrorlist=|g" \
+    -e "s|^#baseurl=http://mirror.centos.org|baseurl=https://mirror.nju.edu.cn/centos-vault|g" \
+    -i.bak \
+    /etc/yum.repos.d/CentOS-*.repo
