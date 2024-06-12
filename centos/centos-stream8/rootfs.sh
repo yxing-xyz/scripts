7491500d09f25fd0c092bb3d677fd7a43bb5fcf7
shawnyyu <shawnyyu@tencent.com>
2024-06-12 15:02:16 +0800

    centos-stream8 repo

--- centos/centos-stream8/rootfs.sh
+++ centos/centos-stream8/rootfs.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 set -e
+# mirror
+sed -e "s|^mirrorlist=|#mirrorlist=|g" \
+    -e "s|^#baseurl=http://mirror.centos.org|baseurl=https://mirror.nju.edu.cn/centos-vault|g" \
+    -i.bak \
+    /etc/yum.repos.d/CentOS-*.repo
+
 yum clean all && yum makecache
 yum update -y
-
 yum install -y epel-release
+sed -e 's!^metalink=!#metalink=!g' \
+    -e 's!^#baseurl=!baseurl=!g' \
+    -e 's!https\?://download\.fedoraproject\.org/pub/epel!https://mirror.nju.edu.cn/epel!g' \
+    -e 's!https\?://download\.example/pub/epel!https://mirror.nju.edu.cn/epel!g' \
+    -i /etc/yum.repos.d/epel*.repo
+yum update -y
+sed -e 's!^metalink=!#metalink=!g' \
+    -e 's!^#baseurl=!baseurl=!g' \
+    -e 's!https\?://download\.fedoraproject\.org/pub/epel!https://mirror.nju.edu.cn/epel!g' \
+    -e 's!https\?://download\.example/pub/epel!https://mirror.nju.edu.cn/epel!g' \
+    -i /etc/yum.repos.d/epel*.repo
+
 yum install -y gcc gcc-c++ make automake autoconf libtool perl bash git lrzsz procps \
     psmisc sudo vim tmux netcat
 yum install -y openssh-server zlib-devel openssl-devel pcre-devel
@@ -15,15 +32,3 @@ ssh-keygen -A
 echo 'root:root' | chpasswd
 useradd -m -s /bin/bash x
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
-## china
-sed -e 's|^mirrorlist=|#mirrorlist=|g' \
-    -e 's|^#baseurl=http://mirror.centos.org/$contentdir|baseurl=https://mirrors.nju.edu.cn/centos|g' \
-    -i.bak \
-    /etc/yum.repos.d/CentOS-*.repo
-
-sed -e 's!^metalink=!#metalink=!g' \
-    -e 's!^#baseurl=!baseurl=!g' \
-    -e 's!https\?://download\.fedoraproject\.org/pub/epel!https://mirror.nju.edu.cn/epel!g' \
-    -e 's!https\?://download\.example/pub/epel!https://mirror.nju.edu.cn/epel!g' \
-    -i /etc/yum.repos.d/epel*.repo
--- centos/centos8/rootfs.sh
+++ centos/centos8/rootfs.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 set -e
+sed -e "s|^mirrorlist=|#mirrorlist=|g" \
+    -e "s|^#baseurl=http://mirror.centos.org|baseurl=https://mirror.nju.edu.cn/centos-vault|g" \
+    -i.bak \
+    /etc/yum.repos.d/CentOS-*.repo
 yum clean all && yum makecache
 yum update -y
+yum install epel-release
+sed -e 's!^metalink=!#metalink=!g' \
+    -e 's!^#baseurl=!baseurl=!g' \
+    -e 's!https\?://download\.fedoraproject\.org/pub/epel!https://mirror.nju.edu.cn/epel!g' \
+    -e 's!https\?://download\.example/pub/epel!https://mirror.nju.edu.cn/epel!g' \
+    -i /etc/yum.repos.d/epel{,-testing}.repo
+yum update -y
+sed -e 's!^metalink=!#metalink=!g' \
+    -e 's!^#baseurl=!baseurl=!g' \
+    -e 's!https\?://download\.fedoraproject\.org/pub/epel!https://mirror.nju.edu.cn/epel!g' \
+    -e 's!https\?://download\.example/pub/epel!https://mirror.nju.edu.cn/epel!g' \
+    -i /etc/yum.repos.d/epel{,-testing}.repo
 
-yum install -y gcc gcc-c++ make automake autoconf libtool perl bash git lrzsz procps epel-release \
+yum install -y gcc gcc-c++ make automake autoconf libtool perl bash git lrzsz procps \
     sudo vim tmux
 yum install -y openssh-server zlib-devel openssl-devel pcre-devel
 yum install -y tcpdump lsof net-tools bind-utils mtr wget curl
@@ -13,15 +29,3 @@ ssh-keygen -A
 echo 'root:root' | chpasswd
 useradd -m -s /bin/bash x
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
-# mirror
-sed -e "s|^mirrorlist=|#mirrorlist=|g" \
-    -e "s|^#baseurl=http://mirror.centos.org|baseurl=https://mirror.nju.edu.cn/centos-vault|g" \
-    -i.bak \
-    /etc/yum.repos.d/CentOS-*.repo
-
-sed -e 's!^metalink=!#metalink=!g' \
-    -e 's!^#baseurl=!baseurl=!g' \
-    -e 's!https\?://download\.fedoraproject\.org/pub/epel!https://mirror.nju.edu.cn/epel!g' \
-    -e 's!https\?://download\.example/pub/epel!https://mirror.nju.edu.cn/epel!g' \
-    -i /etc/yum.repos.d/epel{,-testing}.repo
