7a2f8eb59169bb2dad51abfc94daf6c7af4e3bb0
Ethan <yxing.xyz@gmail.com>
2025-05-24 19:08:57 +0800

    pam

--- rockylinux/8/rootfs.sh
+++ rockylinux/8/rootfs.sh
@@ -9,9 +9,7 @@ yum install -y gcc gcc-c++ make automake autoconf libtool perl bash git lrzsz pr
     psmisc sudo vim tmux netcat glibc-common
 yum install -y openssh-server zlib-devel openssl-devel pcre-devel
 yum install -y tcpdump lsof net-tools bind-utils mtr wget curl glibc-langpack-en glibc-langpack-zh
-echo "zh_CN.UTF-8 UTF-8" >> /etc/locale.gen
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "LANG=en_US.UTF-8" >> /etc/environment
+echo "LANG=en_US.UTF-8" >>/etc/environment
 localedef -i en_US -f UTF-8 en_US.UTF-8
 localedef -i zh_CN -f UTF-8 zh_CN.UTF-8
 sed -i 's/[# ]*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
@@ -20,7 +18,6 @@ ssh-keygen -A
 echo 'root:root' | chpasswd
 useradd -m -s /bin/bash x
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
 
 sed -e 's|^mirrorlist=|#mirrorlist=|g' \
     -e 's|^#baseurl=http://dl.rockylinux.org/$contentdir|baseurl=https://mirror.nju.edu.cn/rocky|g' \
--- rockylinux/9/rootfs.sh
+++ rockylinux/9/rootfs.sh
@@ -6,15 +6,12 @@ yum update -y
 yum install -y epel-release
 yum update -y
 
-
 yum install -y gcc gcc-c++ make automake autoconf libtool perl bash git lrzsz procps \
     psmisc sudo vim tmux netcat glibc-common
 yum install -y openssh-server zlib-devel openssl-devel pcre-devel
-yum install -y tcpdump lsof net-tools bind-utils mtr wget glibc-langpack-en glibc-langpack-zh
+yum install -y tcpdump lsof net-tools bind-utils mtr wget glibc-locale-source glibc-langpack-en glibc-langpack-zh
 yum install -y curl --allowerasing
-echo "zh_CN.UTF-8 UTF-8" >> /etc/locale.gen
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "LANG=en_US.UTF-8" >> /etc/environment
+echo "LANG=en_US.UTF-8" >>/etc/environment
 localedef -i en_US -f UTF-8 en_US.UTF-8
 localedef -i zh_CN -f UTF-8 zh_CN.UTF-8
 sed -i 's/[# ]*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
@@ -22,7 +19,6 @@ ssh-keygen -A
 echo 'root:root' | chpasswd
 useradd -m -s /bin/bash x
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
 
 sed -e 's|^mirrorlist=|#mirrorlist=|g' \
     -e 's|^#baseurl=http://dl.rockylinux.org/$contentdir|baseurl=https://mirror.nju.edu.cn/rocky|g' \
