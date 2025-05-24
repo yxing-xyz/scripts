68a28225dba68f126e575788e15197a63f50c7e9
Ethan <yxing.xyz@gmail.com>
2025-05-24 17:36:48 +0800

    pam

--- rockylinux/8/rootfs.sh
+++ rockylinux/8/rootfs.sh
@@ -8,7 +8,7 @@ yum update -y
 yum install -y gcc gcc-c++ make automake autoconf libtool perl bash git lrzsz procps \
     psmisc sudo vim tmux netcat
 yum install -y openssh-server zlib-devel openssl-devel pcre-devel
-yum install -y tcpdump lsof net-tools bind-utils mtr wget curl
+yum install -y tcpdump lsof net-tools bind-utils mtr wget curl locales
 echo "zh_CN.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/environment
