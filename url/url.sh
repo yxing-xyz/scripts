815c72462e14ed8c33750a2478d5c4588dbfc33b
Ethan Yu <yxing.xyz@gmail.com>
2026-06-13 03:36:00 +0800

    fix: 增加nix容器

--- url/url.sh
+++ url/url.sh
@@ -7,16 +7,48 @@ EOF
 echo "$content" >./url.txt
 echo >>./url.txt
 
-tee >>./url.txt <<EOF
+tee >>./url.txt <<'EOF'
+# 1. 创建 Docker 卷
+docker volume create nix
+docker volume create home
 
-docker run -dit --name code --hostname code --restart always \\
-    --privileged --pull always --platform linux/amd64 \\
-    -p 22:22 \\
-    -e DOCKER_HOST=tcp://host.docker.internal:2375 \\
-    -v home:/home \\
-    registry.cn-hangzhou.aliyuncs.com/yxing-xyz/linux:arch \\
+# 2. 启动 Nix Daemon 容器 (带初始化逻辑)
+docker run -dit \
+    --name nix \
+    --hostname code \
+    --restart always \
+    --privileged \
+    --pull always \
+    --platform linux/amd64 \
+    -v nix:/nix \
+    nixpkgs/nix-flakes bash -c "
+        if [ ! -f /etc/nix/.initialized ]; then
+            rm -f /etc/nix/nix.conf
+            echo 'experimental-features = nix-command flakes' >> /etc/nix/nix.conf
+            echo 'accept-flake-config = true' >> /etc/nix/nix.conf
+            echo 'trusted-users = *' >> /etc/nix/nix.conf
+            touch /etc/nix/.initialized
+            echo 'Nix initialized successfully.'
+        fi
+        nix-daemon
+    "
+
+# 3. 启动开发环境容器 (建议在此处挂载 Socket)
+docker run -dit \
+    --name code \
+    --hostname code \
+    --restart always \
+    --privileged \
+    --pull always \
+    --platform linux/amd64 \
+    -p 22:22 \
+    -e DOCKER_HOST=tcp://host.docker.internal:2375 \
+    -v home:/home \
+    -v nix:/nix \
+    registry.cn-hangzhou.aliyuncs.com/yxing-xyz/linux:arch \
     bash -c "mkdir -p /run/sshd && /usr/sbin/sshd -D"
 EOF
+
 echo >>./url.txt
 RepoLatestRelease() {
     owner=$1
